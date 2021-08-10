1866c5dd94f652b0d94be19f00ab4179560e8cee
linxiazhu <704239108@qq.com>
2021-08-10 11:07:39 +0800

    菜单

--- nhs-sys/src/main/java/com/vblessings/nhs/model/po/business/BusMenuBatchUpdatePO.java
+++ nhs-sys/src/main/java/com/vblessings/nhs/model/po/business/BusMenuBatchUpdatePO.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 
@@ -14,19 +15,21 @@ public class BusMenuBatchUpdatePO {
     /**
      * 开始时间
      */
-    @ApiModelProperty(value = "开始时间")
+    @NotNull(message = "开始时间不能为空")
+    @ApiModelProperty(value = "开始时间", required = true)
     private Date startTime;
 
     /**
      * 结束时间
      */
-    @ApiModelProperty(value = "结束时间")
+    @NotNull(message = "结束时间不能为空")
+    @ApiModelProperty(value = "结束时间", required = true)
     private Date endTime;
 
     /**
-     * 结束时间
+     * 菜谱列表
      */
-    @ApiModelProperty(value = "结束时间")
+    @ApiModelProperty(value = "菜谱列表")
     private List<BusMenuBatchUpdateInfo> busMenuBatchUpdateInfos;
 
 }
--- nhs-sys/src/main/java/com/vblessings/nhs/model/po/business/BusMenuSelectPO.java
+++ nhs-sys/src/main/java/com/vblessings/nhs/model/po/business/BusMenuSelectPO.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -13,12 +14,14 @@ public class BusMenuSelectPO {
     /**
      * 开始时间
      */
-    @ApiModelProperty(value = "开始时间")
+    @NotNull(message = "开始时间不能为空")
+    @ApiModelProperty(value = "开始时间", required = true)
     private Date startTime;
 
     /**
      * 结束时间
      */
-    @ApiModelProperty(value = "结束时间")
+    @NotNull(message = "结束时间不能为空")
+    @ApiModelProperty(value = "结束时间", required = true)
     private Date endTime;
 }
